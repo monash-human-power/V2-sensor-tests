ff8cb7caebcca5eae79ded9ca88813667b7c04d7
khanguslee <khanguslee@gmail.com>
2020-02-26 18:33:09 +1100

    Update teensy script with GPS sensor

--- imu-gps-test/imu-gps-test.ino
+++ imu-gps-test/imu-gps-test.ino
@@ -1,10 +1,20 @@
-#include <SPI.h>
+#include <TinyGPS++.h>
 #include <SparkFunLSM6DS3.h>
 
+#include <SoftwareSerial.h>
+#include <SPI.h>
+
 // Set up Accelerometer
-const int LMS6DS3chipSelect = 6; // CS Pin
+static const int LMS6DS3chipSelect = 6; // CS Pin
 LSM6DS3 myIMU(SPI_MODE, LMS6DS3chipSelect);
 float aX, aY, aZ;
+
+// Set up GPS
+static const int GPS_RXPin = 7, GPS_TXPin = 8;
+static const uint32_t GPS_Baud = 9600;
+
+SoftwareSerial ss(GPS_RXPin, GPS_TXPin);
+TinyGPSPlus tinyGPS;
 
 void setup() {
   Serial.begin(9600);
@@ -14,9 +24,14 @@ void setup() {
   SPI1.setMISO(1); // SAD Pin
   SPI1.setSCK(20); // SCL Pin
   myIMU.begin();
+
+  // Set Up GPS
+  ss.begin(GPS_Baud);
+
 }
 
 void loop() {
+  // Get IMU data
   aX = myIMU.readFloatAccelX();
   aY = myIMU.readFloatAccelY();
   aZ = myIMU.readFloatAccelZ();
@@ -26,5 +41,18 @@ void loop() {
   Serial.println("aY:" + String(aY, 5));
   Serial.println("aZ:" + String(aZ, 5));
   Serial.println();
+
+  // Get GPS data
+  if ((ss.available() > 0)) {
+    if (tinyGPS.encode(ss.read())) {
+      Serial.println("GPS");
+      if (tinyGPS.satellites.value() > 0) {
+        Serial.println("Lat: " + String(tinyGPS.location.lat(), 8));
+        Serial.println("Lon: " + String(tinyGPS.location.lng(), 8));
+      } else {
+        Serial.println("No GPS Satellites");
+      }
+    }
+  }
   delay(100);
 }
