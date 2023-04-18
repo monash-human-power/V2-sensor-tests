a7086b9da7a5a5adc3fc55e6f5e689ec5c4191fe
Arosh Heenkenda <ahee0011@student.monash.edu>
2023-04-18 19:13:44 +1000

    Move local files into git to log work

---
+++ wheel-rpm-logging/pandas_tester.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+
+"""
+Important steps
+
+install latest version of python 
+pip install serial
+pip install xlwt
+pip install openpyxl
+"""
+
+if __name__ == "__main__":
+    pass
