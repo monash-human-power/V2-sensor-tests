cd45db4f890cecb3329955769d7824dd09d3956b
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2023-04-18 19:42:32 +1000

    Add gitignore for excel files, messing around with pandas

--- wheel-rpm-logging/pandas_tester.py
+++ wheel-rpm-logging/pandas_tester.py
@@ -1,14 +1,21 @@
 import os
 import pandas as pd
+import openpyxl
 
 """
 Important steps
 
 install latest version of python 
+pip install pandas
 pip install serial
 pip install xlwt
 pip install openpyxl
 """
 
+def main():
+    df = pd.DataFrame([[11, 21, 31], [12, 22, 32], [31, 32, 33]], columns=['a', 'b', 'c'])
+    df.to_excel('test.xlsx', sheet_name='new_sheet_name')
+
+
 if __name__ == "__main__":
-    pass
+    main()
