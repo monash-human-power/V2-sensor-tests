fbba9887103f9261f25e5f1c4c1e89efc48b8380
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2023-04-18 23:41:39 +1000

    Add comments, small code fixes, add README

--- wheel-rpm-logging/wheel_logger.py
+++ wheel-rpm-logging/wheel_logger.py
@@ -1,8 +1,9 @@
-import serial
-import os
-import openpyxl
-import pandas as pd
 
+#Imports
+import serial
+import xlwt
+from datetime import datetime
+import pandas as pd
 
 
 class WheelLogger:
@@ -19,49 +20,55 @@ class WheelLogger:
         ser = None
         ser_data = []
 
+        #Try to set up serial reader
         try:
             ser = serial.Serial(self.port, self.baud_rate, timeout=1)
 
         except Exception as e:
             print("{}".format(e))
 
+        print("STARTING LOGGING \nPRESS CTRL+C TO STOP\n\n")
 
+        #Read data via serial port
         try:
-
             while True:
                 
+                #Convert data from byte to string
                 data = str(ser.readline().decode()).strip()
 
                 if (len(data)>0) and (data != "Time_us,Rotation_Number,Rotation_Time_us,Angular_V_rad_s,Angular_A_rad_s2,RPM"):
                     
+                    #Format properly and add to data array
                     format_data = list(map(float, data.split(",")))
                     print(format_data)
                     ser_data.append(format_data)
 
-        
+        #Keyboard interrupt
         except KeyboardInterrupt as e:
             print("{}".format(e))
+        
+        #Once we hit CTRL+C we do the rest of the conversion to excel
+        finally:
 
-        print("we're stopping")
-        ser.close()
-        df = pd.DataFrame(ser_data, columns=['Time_us' ,' Rotation_Number', 'Rotation_Time_us', 'Angular_V_rad_s', 'Angular_A_rad_s2', 'RPM'])
-        df.to_excel('test.xlsx', sheet_name='new_sheet_name', index=False)
+            #Close serial connnection
+            ser.close()
+
+            #File name for excel file
+            file_name = self.fname + "_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".xlsx"
+
+            #Append to excel file
+            df = pd.DataFrame(ser_data, columns=['Time_us' ,' Rotation_Number', 'Rotation_Time_us', 'Angular_V_rad_s', 'Angular_A_rad_s2', 'RPM'])
+            df.to_excel(file_name, sheet_name='new_sheet_name', index=False)
+
+            print("\nCTRL+C PRESSED \nWE ARE DONE WITH LOGGING")
     
-
-
-    
-
-
-    
-
-
 
 
 if __name__=="__main__":
 
-    PORT = "COM3"
-    BAUD_RATE = 115200
-    FNAME = "WHEELRIG"
+    PORT = "SOME_PORT" #PORT THAT WHEEL RIG IS CONNECTED TO, CHANGE IF YOU HAVEN'T ALREADY
+    BAUD_RATE = 115200 #BAUD RATE, DON'T CHANGE UNLESS NECESSARY
+    FNAME = "WHEELRIG" #FILENAME CONVENTION
 
     wheelLogger = WheelLogger(PORT, BAUD_RATE, FNAME)
     wheelLogger.run()
