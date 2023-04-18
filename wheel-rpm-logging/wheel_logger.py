0e83128bf03b46ef45fb1771cfddf90ec62f1f55
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2023-04-18 20:39:21 +1000

    Read serial data in correct format

--- wheel-rpm-logging/wheel_logger.py
+++ wheel-rpm-logging/wheel_logger.py
@@ -25,13 +25,23 @@ class WheelLogger:
         except Exception as e:
             print("{}".format(e))
 
+
         try:
 
             while True:
-                print("hey")
+                
+                data = str(ser.readline().decode()).strip()
+
+                if (len(data)>0) and (data != "Time_us,Rotation_Number,Rotation_Time_us,Angular_V_rad_s,Angular_A_rad_s2,RPM"):
+
+                    print(data.split(","))
+
         
         except KeyboardInterrupt as e:
             print("{}".format(e))
+
+        print("we're stopping")
+        ser.close()
 
     
 
